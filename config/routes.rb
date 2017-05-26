90b7a8dac51563f13a540d2daf454525b180013d
DIANA JURGIELEWICZ <DMJURG@GMAIL.com>
2017-05-26 16:59:31 -0400

    adds devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :posts do
     resources :comments
   end
@@ -6,5 +7,5 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   get '/about', to: 'pages#about'
-  
+
 end
